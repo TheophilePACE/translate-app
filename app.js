65b48d1783cc3eaa9e864bdde954f6817467bf23
TheophilePACE <theophile.pace@gmail.com>
2017-09-26 02:23:33 +1000

    Fix main path

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var translation = require('./routes/translation');
 
 var app = express();
@@ -22,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', translation);
 app.use('/translation', translation);
 
 // catch 404 and forward to error handler
